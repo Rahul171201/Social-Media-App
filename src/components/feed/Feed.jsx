e07270bd99435d4babfa6ff940014bc37ae86863
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2022-05-14 10:51:20 +0530

    posts fetched from beckend server

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -2,14 +2,29 @@ import './feed.css';
 import Share from '../../components/share/Share';
 import Post from '../../components/post/Post';
 import {Posts} from '../../dummyData';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 export default function Feed() {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async() => {
+            const res = await axios.get('http://localhost:8800/api/post/timeline/623314c2a4b89e305cc29f1e');
+            console.log(res.data);
+            setPosts(res.data);
+        }
+        fetchPosts();
+    }, []);
+
     return (
         <div className="feed">
             <div className = "feedWrapper">
                 <Share></Share>
-                {Posts.map((p) =>{
-                    return <Post key={p.id} post={p}></Post>
+                {posts.map((p) =>{
+                    return <Post key={p._id} post={p}></Post>
                 })}
             </div>
         </div>
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,21 +1,25 @@
 import './post.css'
 import { More, MoreVert } from '@material-ui/icons';
-import { Users } from '../../dummyData';
-import { useState } from 'react';
+// import { Users } from '../../dummyData';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { format } from 'timeago.js';
 
 export default function Post({ post }) {
 
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-
-    let userName = "";
-    let userImage = "";
-    Users.forEach((element) => {
-        if (element.id === post.userId) {
-            userImage = element.profilePic;
-            userName = element.username;
-        }
-    });
     const [noOfLikes, setNoOfLikes] = useState(0);
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        const fetchUser = async() => {
+            console.log("New Post : " + post.userId);
+            const res = await axios.get(`http://localhost:8800/api/user/${post.userId}`);
+            console.log(res.data);
+            setUser(res.data);
+        }
+        fetchUser();
+    }, [post.userId]);
 
     function handleLike() {
         if (noOfLikes === 0) {
@@ -26,16 +30,14 @@ export default function Post({ post }) {
         }
     }
 
-    
-
     return (
         <div className="postContainer">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img src={PF + userImage} className="postProfileImage" alt=""></img>
-                        <span className="postUsername">{userName}</span>
-                        <span className="postTime">{post.date}</span>
+                        <img src={user.profilePic ? PF + user.profilePic : PF + "blackprofile.webp"} className="postProfileImage" alt=""></img>
+                        <span className="postUsername">{user.username}</span>
+                        <span className="postTime">{format(post.createdAt)}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert className="showMore"></MoreVert>
@@ -43,13 +45,13 @@ export default function Post({ post }) {
                 </div>
                 <div className="postCenter">
                     <div className="postText">{post.desc}</div>
-                    <img className="postImage" src={PF + post.img} alt="post image"></img>
+                    <img className="postImage" src={post.img ? PF + post.img : PF + 'blankpost.png'} alt="post"></img>
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         <img src="/assets/heart.png" alt="" className="heartIcon"></img>
                         <img src="/assets/like.png" alt="" className="likeIcon" onClick={handleLike}></img>
-                        <span className="likeCounter">{post.like + noOfLikes} people liked it</span>
+                        <span className="likeCounter">{post.like ? post.like + noOfLikes : 0 + noOfLikes} people liked it</span>
                     </div>
                     <div className="postBottomRight">
                         <span className="postCommentText">
